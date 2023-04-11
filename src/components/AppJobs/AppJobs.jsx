bb91e825e68e617531611a2869a8da95345a8444
Tanvirsayem99 <107928205+Tanvirsayem99@users.noreply.github.com>
2023-04-11 14:13:20 +0600

    remote and onsite filter included in applied job pages

--- src/components/AppJobs/AppJobs.jsx
+++ src/components/AppJobs/AppJobs.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { getStoredData } from '../../Utils/FakeDb';
 import StoredJobs from '../StoredJobs/StoredJobs';
@@ -6,19 +6,28 @@ import StoredJobs from '../StoredJobs/StoredJobs';
 const AppJobs = () => {
     const storedJobs = useLoaderData();
     let cart = []
+    const [afra, setAfra] = useState(cart)
     const sayem = getStoredData()
-    for(const id in sayem){
-        const storedItems = storedJobs.find(e => e.id === id)
-        if(storedItems){
-            cart.push(storedItems)
-            console.log('hello')
+    
+    
+        for(const id in sayem){
+            const storedItems = storedJobs.find(e => e.id === id)
+            if(storedItems){
+                cart.push(storedItems)
+            }
         }
-        else{
-            console.log('hi')
-        }
-        console.log(storedItems)
-        
+
+
+    
+    const clicker =() =>{
+        const filterOnsite = cart.filter(e => e.remoteOrOnsite.status === 'Onsite');
+        console.log(filterOnsite)
+        setAfra(filterOnsite)
     }
+
+
+    
+
     
 
     
@@ -27,9 +36,13 @@ const AppJobs = () => {
             <div className='bg-slate-200 h-[25vh] flex justify-center items-center'>
                 <h1 className='text-center font-semibold text-2xl'>Applied Jobs</h1>
             </div>
+            <div className='flex gap-10'>
+                <button>remote</button>
+                <button onClick={()=>clicker()}>onsite</button>
+            </div>
             <div>
             {
-                cart.map(e => (<StoredJobs key={e.id} e={e}></StoredJobs>))
+                afra.map(e => (<StoredJobs key={e.id} e={e}></StoredJobs>))
             }
         </div>
         </div>
